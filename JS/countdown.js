6b5e9eb177203baa2c3933c30cc76111ac1f54d8
Michael Rodriguez <rodrigum3604@student.faytechcc.edu>
2022-09-29 11:32:44 -0400

    aoacountdownpush

---
+++ JS/countdown.js
@@ -0,0 +1,33 @@
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      //set day/month here
+      dayMonth = "12/12/",
+      birthday = dayMonth + yyyy;  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached like show ip
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "here the ip or something";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
